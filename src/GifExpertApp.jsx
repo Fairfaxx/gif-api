13b6d9f4a5c54fc30c8fe4b88e3b12ef6b253b74
FefeLemaire <federico.lemaire@alloy.digital>
2022-12-02 14:45:53 -0300

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GifExpertApp = () => {
+  return (
+    <>
+      <h1>GifExpert App</h1>
+      <p>GifExpert is a free and open source project to develop GifExpert.</p>
+    </>
+  );
+};
+
+export default GifExpertApp;
